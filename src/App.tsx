43f5e64c1542ca90404c3e12cf948856ed4bfbb9
jairodoni <jairo.doni97@gmail.com>
2021-10-29 16:33:54 -0300

    Migration for Vite version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Routes } from './routes';
+import { Navigation } from "./components/Navigation";
 
-function App() {
-  return <Routes />;
+import developer from './assets/images/developer.svg'
+import waves from './assets/images/waves.svg'
+import styles from './styles/home.module.scss';
+import { Curriculum } from "./components/Curriculum";
+
+export default function App() {
+  return (
+    <>
+      <main className={styles.main_container}>
+        <Navigation />
+        <section className={styles.front_cover}>
+
+          <img className={styles.developer} src={developer} alt="developer" />
+
+          <img className={styles.waves} src={waves} alt="waves" />
+        </section>
+        <Curriculum />
+      </main>
+    </>
+  );
 }
-
-export default App;
--- src/components/Curriculum/index.tsx
+++ src/components/Curriculum/index.tsx
@@ -19,10 +19,22 @@ export function Curriculum() {
           e procurar desenvolver soluções inovadoras.
         </p>
         <h3>Experiencia com:</h3>
-        <div>
+        {/* <div>
           <label>Linguagens:</label>
           <p>HTML, Javascript (Typescript, ReactJS, Next.js, React Native, Node.js), CSS (SASS, Styled Component), Jest.</p>
+        </div> */}
+        <div >
+          <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" />
+          <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&&logo=typescript&logoColor=white" />
+          <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&&logo=react&logoColor=61DAFB" />
+          <img src="https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
+          <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />
+          <img src="https://img.shields.io/badge/-HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+          <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+          <img src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white" />
+          <img src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white" />
         </div>
+
         <div>
           <label>Banco de Dados:</label>
           <p>MySQL, SQLite, MongoDB.</p>
@@ -34,9 +46,9 @@ export function Curriculum() {
         <a className={styles.button} href="#">Ver Curriculo Completo</a>
       </div>
 
-      <div className={styles.photo}>
+      {/* <div className={styles.photo}>
         <img src="https://images.unsplash.com/photo-1611530437165-1a02c8d44532?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=627&q=80" alt="Photo" />
-      </div>
+      </div> */}
     </div>
   )
 }
