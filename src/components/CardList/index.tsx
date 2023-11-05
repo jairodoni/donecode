2f0e3b1472fdc79850cc6f31061667cc63a84659
Jairo Doni <jairo.doni97@gmail.com>
2023-11-05 20:00:00 -0300

    animation ao abrir e fechar o ver mais dos softwares

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 import { useProfile } from '../../hooks/useProfile'
 import { Project } from '../../types/profileContextTypes'
 import { PiGearSixFill } from 'react-icons/pi'
+import { motion } from 'framer-motion'
 import styles from './styles.module.scss'
 import { Tooltip } from '../Tooltip'
+import { useState } from 'react'
 
 interface CardListProps {
   projects: Project[]
@@ -13,46 +15,18 @@ interface CardListProps {
 }
 
 export function CardList({
-  seeMore = true,
+  seeMore = false,
   projects,
   projectsLimited,
 }: CardListProps) {
   const { getProject } = useProfile()
 
+  const animationSeeMore = seeMore ? styles.seeMore : styles.seeLess
+
   return (
-    <div className={styles.cardList}>
-      {!seeMore &&
-        projectsLimited?.map((project) => (
-          <div
-            key={project.id}
-            onClick={() => getProject(project)}
-            className={styles.card}
-          >
-            <div className={styles.imageCard}>
-              <Image
-                src={project.image_url}
-                alt={project.title}
-                quality={80}
-                width={400}
-                height={200}
-              />
-            </div>
-            <div className={styles.info}>
-              <div>
-                <h4>{project.title}</h4>
-                <p>{project.description}</p>
-              </div>
-              {project.technologies?.slice(0, 5).map((tech) => (
-                <div key={tech.name} className={styles.stack}>
-                  <PiGearSixFill size={16} />
-                  <span>{tech.name}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      {seeMore &&
-        projects.map((project) => (
+    <>
+      <div className={styles.cardList}>
+        {projectsLimited?.map((project) => (
           <div
             key={project.id}
             onClick={() => getProject(project)}
@@ -83,6 +57,44 @@ export function CardList({
             </div>
           </div>
         ))}
-    </div>
+      </div>
+      <div
+        // eslint-disable-next-line @typescript-eslint/no-empty-function, prettier/prettier
+        className={`${animationSeeMore} ${styles.cardList}`}
+      >
+        {seeMore &&
+          projects.map((project) => (
+            <div
+              key={project.id}
+              onClick={() => getProject(project)}
+              className={styles.card}
+            >
+              <div className={styles.imageCard}>
+                <Image
+                  src={project.image_url}
+                  alt={project.title}
+                  quality={80}
+                  width={400}
+                  height={200}
+                />
+              </div>
+              <div className={styles.info}>
+                <div>
+                  <h4>{project.title}</h4>
+                  <p>{project.description}</p>
+                </div>
+                <div className={styles.stackList}>
+                  {project.technologies?.slice(0, 5).map((tech) => (
+                    <div key={tech.name} className={styles.stack}>
+                      <PiGearSixFill size={16} />
+                      <span>{tech.name}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   )
 }
--- src/components/SoftwaresOnline/index.tsx
+++ src/components/SoftwaresOnline/index.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 import { useProfile } from '../../hooks/useProfile'
 import { CardList } from '../CardList'
 
@@ -6,11 +7,43 @@ import styles from './styles.module.scss'
 
 export function SoftwaresOnline() {
   const { deploys } = useProfile()
+  const [seeMore, setSeeMore] = useState(false)
+
+  function handleSeeMore() {
+    setSeeMore(!seeMore)
+  }
 
   return (
-    <section id="softwares" className={styles.container}>
-      <h3>Websites</h3>
-      <CardList projects={deploys} />
-    </section>
+    <>
+      <div className={styles.borderBlack} />
+      <section id="softwares" className={styles.container}>
+        <h3>Websites</h3>
+        <CardList
+          projects={deploys}
+          projectsLimited={deploys}
+          seeMore={seeMore}
+        />
+        <div className={styles.buttonSeeMore}>
+          {!seeMore ? (
+            <button
+              type="button"
+              className={styles.buttonSeeMore}
+              onClick={handleSeeMore}
+            >
+              Ver mais
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.buttonSeeMore}
+              onClick={handleSeeMore}
+            >
+              Ver menos
+            </button>
+          )}
+        </div>
+      </section>
+      <div className={styles.borderBlack} />
+    </>
   )
 }
